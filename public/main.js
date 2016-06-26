3cb7abe71dfda6213b8b210b165c33edf26704a6
rmadhankv3 <rmadhankv3@gmail.com>
2016-06-26 22:06:08 +0530

    main revert back

--- public/main.js
+++ public/main.js
@@ -11,21 +11,24 @@ var http = require('http');
 var pg = require('pg');
 
 pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+function getData(){
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+      if (err) throw err;
+      console.log('Connected to postgres! Getting schemas...');
 
-  client
-    .query('SELECT firstname,lastname FROM salesforce.contact WHERE firstname = \'Andy\';')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
+      client
+        .query('SELECT firstname,lastname, email FROM salesforce.contact WHERE firstname = \'Andy\';')
+        .on('row', function(row) {
+          console.log(JSON.stringify(row));
+        });
     });
-});
+}
 
 http.createServer(onRequest).listen(process.env.PORT || 3000);
 
 function onRequest(request, response){
     console.log('the request from user',request.url);
+    getData();
     response.writeHead(200);
     response.write('this is a test message');
     response.end();
