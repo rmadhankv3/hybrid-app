3dc6cb46265dbf91426313b5402e23c3bd58026b
rmadhankv3 <rmadhankv3@gmail.com>
2016-06-26 19:20:39 +0530

    new app practice 2

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@ console.log('started node app');
 
 var http = require('http');
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(process.env.PORT || 3000);
 
 function onRequest(request, response){
     console.log('the request from user',request.url);
