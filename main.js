88c4bef3d850e22de535d5721db6e112d4a80834
rmadhankv3 <rmadhankv3@gmail.com>
2016-06-26 15:26:00 +0530

    main functionality added

--- main.js
+++ main.js
@@ -3,5 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var http = require("http");
 
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("It's alive!");
+  response.end();
+}).listen(3000);
 
