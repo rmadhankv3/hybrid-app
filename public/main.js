3dc0bc597582673d3f94a9da5a442fdaa92543d2
rmadhankv3 <rmadhankv3@gmail.com>
2016-06-26 21:35:29 +0530

    postgres 1st functionality

--- public/main.js
+++ public/main.js
@@ -3,9 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/* global process */
+
 console.log('started node app');
 
 var http = require('http');
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT firstname,lastname FROM salesforce.contact;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 http.createServer(onRequest).listen(process.env.PORT || 3000);
 
