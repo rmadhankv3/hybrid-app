88e4f7fb19214ced1dc156c9b5bd2062f405d258
rmadhankv3 <rmadhankv3@gmail.com>
2016-06-26 16:07:57 +0530

    new line in main

--- main.js
+++ main.js
@@ -3,11 +3,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!!!!!!!!!!!!!");
-  response.end();
-}).listen(3000);
+console.log('started node app');
 
