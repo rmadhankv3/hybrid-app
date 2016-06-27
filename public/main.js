f231b8f3e338da5346637b083fba5b614c3b3ebe
rmadhankv3 <rmadhankv3@gmail.com>
2016-06-27 08:12:13 +0530

    updating with new code in main

--- public/main.js
+++ public/main.js
@@ -11,26 +11,39 @@ var http = require('http');
 var pg = require('pg');
 
 pg.defaults.ssl = true;
-function getData(){
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
+
+//pg.connect(process.env.DATABASE_URL, function(err, client) {
+function getData(response){
+    pg.connect('postgres://mpxytmhoxdycpa:Q8qjZJB1zlqueEnH2KeOAA2X8H@ec2-54-235-208-3.compute-1.amazonaws.com:5432/d6jl4fnfe9f22m', function(err, client) {
       if (err) throw err;
       console.log('Connected to postgres! Getting schemas...');
 
       client
-        .query('SELECT firstname,lastname, email FROM salesforce.contact WHERE firstname = \'Andy\';')
+        .query('SELECT firstname,lastname, email FROM salesforce.contact;')
         .on('row', function(row) {
-          console.log(JSON.stringify(row));
+          //console.log(JSON.stringify(row));
+          //response.write(JSON.stringify(row));
+          response.write('First Name : '+row.firstname+'\n');
+          response.write('Last Name : '+row.lastname+'\n');
+          response.write('Email : '+row.email+'\n\n');
+          //response.end();
+        })
+        .on('end',function(ele){
+            //console.log(ele);
+            response.end();
         });
     });
 }
+
 
 http.createServer(onRequest).listen(process.env.PORT || 3000);
 
 function onRequest(request, response){
     console.log('the request from user',request.url);
-    getData();
+    
     response.writeHead(200);
-    response.write('this is a test message');
-    response.end();
+    response.write('this is a test message\n');
+    getData(response);
+    //response.end();
 }
 
